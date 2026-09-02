02fbeae02cd06d63fc72944a987f5779655c2bfe
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-09-02 12:00:36 -0300

    fix: resolve linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,8 @@ export default defineConfig([
 		extends: [eslintConfigInclusiveDesign],
 		rules: {
 			'new-cap': ['error', { capIsNewExceptionPattern: '^Elena' }],
+			'no-constant-binary-expression': 'off',
+			'unicorn/no-top-level-side-effects': 'off',
 		},
 	},
 	{
